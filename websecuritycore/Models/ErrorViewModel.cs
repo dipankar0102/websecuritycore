1cb5c550bb8ab89711a0c16bbfeb3c833be706a8
dipankar <dipankargarai420@gmail.com>
2023-08-19 12:49:08 +0530

    Add project files.

---
+++ websecuritycore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace websecuritycore.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
